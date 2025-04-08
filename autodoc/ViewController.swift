5def6b2f47d14c049f00817d6fcafe3768e1110b
Artem.Zaitsev <artem.zaytsev@onetwotrip.com>
2025-04-08 16:48:53 +0200

    Initial Commit

---
+++ autodoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autodoc
+//
+//  Created by Артём Зайцев on 08.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
